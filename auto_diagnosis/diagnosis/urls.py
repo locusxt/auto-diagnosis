224333dd0e2227ab4170286e446928fd28fc662f
ZhuTing <locusxt@163.com>
2015-05-30 23:33:59 +0800

    module select finished

--- auto_diagnosis/diagnosis/urls.py
+++ auto_diagnosis/diagnosis/urls.py
@@ -4,4 +4,5 @@ from diagnosis import views
 urlpatterns = patterns('',
                         url(r'^test/$', views.start_diagnosis, name='test'),
                         url(r'^test/ajax/get_states/$', views.get_states),
+                        url(r'^test/ajax/get_modules/$', views.get_modules),
 )
--- auto_diagnosis/diagnosis/views.py
+++ auto_diagnosis/diagnosis/views.py
@@ -13,3 +13,14 @@ def start_diagnosis(request):
 def get_states(request):
     response_data = {'states':['发烧', '头痛', '咳嗽', '头晕', '乏力', '背痛']}
     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+@csrf_exempt
+def get_modules(request):
+	if not request.is_ajax():
+		return HttpResponse("Not Ajax")
+	if request.method != 'POST':
+		return HttpResponse("Not Ajax Post")
+	d = json.loads(request.body)
+	print(d)
+	response_data = {'questions':[{'question' : 'hello?', 'options' : ['a', 'b', 'c']},{'question' : 'world?', 'options' : ['e', 'f', 'g']}]}
+	return HttpResponse(json.dumps(response_data), content_type="application/json")
